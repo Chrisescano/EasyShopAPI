f26210a5f0360c3850bebda5dc318bf14b2cf59d
Christian Escano <escanochristian21@gmail.com>
2024-01-01 16:54:25 -0500

    Created - OrdersController class containing basic boilerplate code

---
+++ src/main/java/org/yearup/controllers/OrdersController.java
@@ -0,0 +1,17 @@
+package org.yearup.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("orders")
+@CrossOrigin
+public class OrdersController {
+
+    @PostMapping()
+    public void checkout() {
+
+    }
+}
