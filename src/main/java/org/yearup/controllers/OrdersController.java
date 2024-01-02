31ec4b9a7aa57a5dd9e5dafab3a198afd0bcdaf9
Christian Escano <escanochristian21@gmail.com>
2024-01-01 22:29:13 -0500

    Added@OrdersController - classes needed to update Orders and OrderLineItems tables

--- src/main/java/org/yearup/controllers/OrdersController.java
+++ src/main/java/org/yearup/controllers/OrdersController.java
@@ -1,17 +1,42 @@
 package org.yearup.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.yearup.data.ProfileDao;
+import org.yearup.data.ShoppingCartDao;
+import org.yearup.data.UserDao;
 
 @RestController
 @RequestMapping("orders")
 @CrossOrigin
+@PreAuthorize("isAuthenticated()")
 public class OrdersController {
+    private ProfileDao profileDao;
+    private UserDao userDao;
+    private ShoppingCartDao shoppingCartDao;
+
+
+    @Autowired
+    public OrdersController(
+            ProfileDao profileDao,
+            UserDao userDao,
+            ShoppingCartDao shoppingCartDao
+    ) {
+        this.profileDao = profileDao;
+        this.userDao = userDao;
+        this.shoppingCartDao = shoppingCartDao;
+    }
 
     @PostMapping()
     public void checkout() {
-
+        //retrieve the users shopping cart
+        //create and insert a new order into orders tables
+        //create a orderlineitem for each shopping cart item
+        //add each item to the database
+        //once created clear the shopping cart
     }
 }
